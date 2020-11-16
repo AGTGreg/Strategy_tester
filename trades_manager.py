cfa56109180d68bd1e1899c48f2f9a9553a4c6b4
Greg Chatzinikolaou <greghatzis@gmail.com>
2020-11-16 15:23:08 +0200

    Remove unused code .

--- trades_manager.py
+++
@@ -1,8 +0,0 @@
-import gc_globals as _globals
-from models import Order
-
-
-def new_buy_order(symbol_name, price):
-    new_order = Order('BUY', symbol_name, price)
-    _globals.BUY_ORDERS.append(new_order)
-    return new_order
